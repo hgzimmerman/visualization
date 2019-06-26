77eaf7c1e894ea41096e3fc78eb836af9a2c314b
Henry Zimmerman <zimhen7@gmail.com>
2019-06-26 16:09:20 -0400

    made an attempt at working with continuous hilbert curves

--- hilbert/src/hilbert.rs
+++ hilbert/src/hilbert.rs
@@ -1,3 +1,5 @@
+use nannou::geom::Point2;
+use std::collections::VecDeque;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub struct Point {
@@ -128,3 +130,69 @@ impl Iterator for HilbertIterator {
     }
 }
 
+
+/// http://bit-player.org/2013/mapping-the-hilbert-curve
+/// Maps a number between 0 and 1 to a two dimensional point.
+/// N [0, 1)
+fn hilbret_map(n: f32) -> Point2 {
+
+
+    let s = format!("{}", n);
+    let s = s.split(".").collect::<Vec<_>>();
+
+    let integer: &str = s[0];
+    let fractional: &str = s.get(1).unwrap_or(&"0");
+
+
+    fn to_digits(mut n: usize, b: usize) -> VecDeque<usize> {
+        let mut digits = VecDeque::new();
+        while n > 0 {
+            digits.push_front(n % b);
+            n  = n / b;
+        }
+        digits
+    }
+
+    fn from_digits(digits: &str, b: usize) -> usize {
+        let mut n = 0;
+        for d in digits.chars() {
+            n = b * n + d.to_digit(10).unwrap() as usize
+        }
+        n
+    }
+
+    let mut integer: VecDeque<u8> = to_digits(integer.parse::<usize>().unwrap(), 4).into_iter().map(|x| x as u8).collect();
+    let fractional: VecDeque<u8> = to_digits(fractional.parse::<usize>().unwrap(), 4).into_iter().map(|x| x as u8).collect();
+    dbg!((&integer, &fractional));
+    integer.extend(fractional);
+    let mut quadits = integer;
+
+
+    /// A quadit is represented by the digits {0, 1, 2, 3}
+    fn hilbert_map_inner(quadits: &mut VecDeque<u8>) -> Point2 {
+        if quadits.len() == 0 {
+            Point2::from((0.5, 0.5))
+        } else {
+            let t = quadits.pop_front().unwrap();          // get first quadit
+            let pt = hilbert_map_inner(quadits);     // recursive call
+            let x = pt.x;
+            let y = pt.y;
+            match t {
+                0 => Point2::from((y * 0.5, x * 0.5)),
+                1 => Point2::from((x * 0.5, y * 0.5 + 0.5)),
+                2 => Point2::from((x * 0.5 + 0.5, y * 0.5 + 0.5)),
+                3 => Point2::from((y * -0.5 + 1.0, x * -0.5 + 0.5)),
+                _ => panic!("should never get a number not in set of {0, 1, 2, 3}")
+            }
+        }
+    }
+
+    hilbert_map_inner(&mut quadits)
+}
+
+#[test]
+fn hib_map() {
+    assert_eq!(hilbret_map(0.0), Point2::from((0.5, 0.5)));
+    assert_eq!(hilbret_map(4.0/17.0), Point2::from((0.0, 0.4)));
+//    assert_eq!(hilbret_map(0.25), Point2::from((0.5, 0.5)));
+}
